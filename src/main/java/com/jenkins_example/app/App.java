ee8fdf7868ec0e2edb6742292f2da317a1d62d22
Ashwamedh44 <ashwamedh.datta@ltts.com>
2024-08-08 11:23:41 +0530

    Update App.java

--- src/main/java/com/jenkins_example/app/App.java
+++ src/main/java/com/jenkins_example/app/App.java
@@ -3,7 +3,9 @@ public class App {
 
     private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+    public App() {
+        // creating a class
+    }
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
