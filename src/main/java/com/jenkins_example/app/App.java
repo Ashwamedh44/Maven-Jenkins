a4539d1df5e1ca287e7e712d5c17b0e6df179a46
Ashwamedh44 <ashwamedh.datta@ltts.com>
2024-08-08 11:09:02 +0530

    Update App.java

--- src/main/java/com/jenkins_example/app/App.java
+++ src/main/java/com/jenkins_example/app/App.java
@@ -1,8 +1,4 @@
 package com.jenkins_example.app;
-
-/**
- * Hello world!
- */
 public class App {
 
     private static final String MESSAGE = "Hello World!";
@@ -10,7 +6,10 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
     }
 
     public String getMessage() {
